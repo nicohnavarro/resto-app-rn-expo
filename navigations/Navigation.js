4c1b860aee9bf429656aa4db9e0f4afcfe844def
nicohnavarro <nicohnavarro@gmail.com>
2021-03-18 14:47:16 -0300

    :sparkles: add icons in tab nav

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -7,13 +7,53 @@ import TopRestaurantsStack from "./TopRestaurantsStack";
 import SearchStack from "./SearchStack";
 import FavoritesStack from "./FavoritesStack";
 import AccountStack from "./AccountStack";
+import { Icon } from "react-native-elements";
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
+
+  const screenOptions = (route,color) =>{
+    let iconName
+    switch (route.name) {
+      case "restaurants":
+          iconName= "compass-outline"
+        break;
+      case "favorites":
+          iconName= "heart-outline"
+        break;
+      case "topRestaurants":
+          iconName= "star-outline"
+        break;
+      case "search":
+          iconName= "magnify"
+        break;
+      case "account":
+          iconName= "home-outline"
+        break;
+    }
+    return (
+      <Icon
+        type="material-community"
+        name={iconName}
+        size={22}
+        color={color}
+      />
+    )
+  }
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="restaurants"
+        tabBarOptions={{
+          inactiveTintColor:"#777b7e",
+          activeTintColor:"#a3bf45"
+        }}
+        screenOptions={({route})=>({
+          tabBarIcon: ({color}) => screenOptions(route,color)
+        })}
+      >
         <Tab.Screen name="restaurants" 
           component={RestaurantsStack}
           options={{title:"Restaurantes"}} />
