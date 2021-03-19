12baffefbabdb402d01d2472b0c45803c847948c
nicohnavarro <nicohnavarro@gmail.com>
2021-03-19 16:59:26 -0300

    :hammer: validations for firebase

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,17 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Input, Button } from "react-native-elements";
+import { Input, Button, Icon } from "react-native-elements";
+import { size } from "lodash";
+
+import { validateEmail } from "../../utils/helpers";
 
 export default function RegisterForm() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState(defaultFormValues());
+  const [errorCorreo, setErrorCorreo] = useState("");
+  const [errorClave, setErrorClave] = useState("");
+  const [errorConfirm, setErrorConfirm] = useState("");
+
+  const onChange = (e, type) => {
+    setFormData({ ...formData, [type]: e.nativeEvent.text });
+  };
+
+  const register = () => {
+    if (!validateData()) {
+      return console.log("nop");
+    }
+
+    console.log("yh");
+  };
+
+  const validateData = () => {
+    setErrorCorreo("");
+    setErrorClave("");
+    setErrorConfirm("");
+    let isValid = true;
+
+    if (!validateEmail(formData.correo)) {
+      setErrorCorreo("Debes ingresar un correo valido.");
+      isValid = false;
+    }
+
+    if (size(formData.clave) < 6) {
+      setErrorClave("Debes ingresar una clave mayor a 6 caracteres.");
+      isValid = false;
+    }
+
+    if (formData.clave !== formData.confirm) {
+      setErrorClave("Las claves deben ser iguales");
+      setErrorConfirm("Las claves deben ser iguales");
+      isValid = false;
+    }
+
+    return isValid;
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <View style={styles.form}>
-      <Input containerStyle={styles.input} placeholder="Ingrese su correo..." />
-      <Input containerStyle={styles.input} password={true} secureTextEntry={true} placeholder="Ingrese su clave..." />
-      <Input containerStyle={styles.input} password={true} secureTextEntry={true} placeholder="Confirme su clave..." />
-      <Button containerStyle={styles.btnRegister} buttonStyle={styles.btn} title="Registar" />
+      <Input
+        containerStyle={styles.input}
+        placeholder="Ingrese su correo..."
+        keyboardType="email-address"
+        onChange={(e) => onChange(e, "correo")}
+        errorMessage={errorCorreo}
+        defaultValue={formData.correo}
+      />
+      <Input
+        containerStyle={styles.input}
+        password={true}
+        secureTextEntry={showPassword ? false : true}
+        placeholder="Ingrese su clave..."
+        onChange={(e) => onChange(e, "clave")}
+        rightIcon={
+          <Icon
+            onPress={() => setShowPassword(!showPassword)}
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.icon}
+          />
+        }
+        errorMessage={errorClave}
+        defaultValue={formData.clave}
+      />
+      <Input
+        containerStyle={styles.input}
+        password={true}
+        secureTextEntry={showPassword ? false : true}
+        placeholder="Confirme su clave..."
+        onChange={(e) => onChange(e, "confirm")}
+        rightIcon={
+          <Icon
+            onPress={handleShowPassword}
+            type="material-community"
+            name={showPassword ? "eye-off-outline" : "eye-outline"}
+            iconStyle={styles.icon}
+          />
+        }
+        errorMessage={errorConfirm}
+        defaultValue={formData.confirm}
+      />
+      <Button
+        containerStyle={styles.btnRegister}
+        buttonStyle={styles.btn}
+        title="Registar"
+        onPress={() => register()}
+      />
     </View>
   );
 }
+
+const defaultFormValues = () => {
+  return { correo: "", clave: "", confirm: "" };
+};
 
 const styles = StyleSheet.create({
   form: {
@@ -20,12 +118,15 @@ const styles = StyleSheet.create({
   input: {
     width: "100%",
   },
-  btnRegister:{
+  btnRegister: {
     marginTop: 20,
     width: "90%",
-    alignSelf:"center"
+    alignSelf: "center",
   },
-  btn:{
-    backgroundColor: "#438282"
-  }
+  btn: {
+    backgroundColor: "#438282",
+  },
+  icon: {
+    color: "#c1c1c1",
+  },
 });
--- screens/account/Register.js
+++ screens/account/Register.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import RegisterForm from '../../components/account/RegisterForm';
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import RegisterForm from "../../components/account/RegisterForm";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
 export default function Register() {
-    return (
-        <View>
-            <RegisterForm></RegisterForm>    
-        </View>
-    )
+  return (
+    <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+      <Image
+        source={require("../../assets/logo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <RegisterForm></RegisterForm>
+    </KeyboardAwareScrollView>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        textAlign:"center",
+        justifyContent:"center",
+        marginTop:10
+    },
+    image: {
+        height: 150,
+        width: "100%",
+        marginBottom: 20,
+      },
+});
