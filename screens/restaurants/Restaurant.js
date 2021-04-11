7a2cdda3550d25ce52e9d5940d83697891a2c3ba
nicohnavarro <nicohnavarro@gmail.com>
2021-04-10 23:57:22 -0300

    :fire: restaurant description

--- screens/restaurants/Restaurant.js
+++ screens/restaurants/Restaurant.js
@@ -1,13 +1,35 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Alert } from "react-native";
+import Loading from "../../components/Loading";
+import { getDocumentById } from "../../utils/actions";
 
 export default function Restaurant({ navigation, route }) {
   const { id, name } = route.params;
+  const [restaurant, setRestaurant] = useState(null);
 
-  navigation.setOptions({title:name})
+  navigation.setOptions({ title: name });
+
+  useEffect(() => {
+    async () => {
+      const response = await getDocumentById("restaurants", id);
+      if (response.statusResponse === true) {
+        setRestaurant(response.document);
+      } else {
+        setRestaurant({});
+        Alert.alert(
+          "Ocurrio un problema cargando el restaurante, intente mas tarde."
+        );
+      }
+    };
+  }, []);
+
+  if (!restaurant) {
+    return <Loading isVisible={true} text="cargando..." />;
+  }
+
   return (
     <View>
-      <Text>Restaurant</Text>
+      <Text>{restaurant.description}</Text>
     </View>
   );
 }
